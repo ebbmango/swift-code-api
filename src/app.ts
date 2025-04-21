4414fb1bf66ac231e3832dfd901e97f2fb88de1a
Emanuel Borges <ebbmango@tuta.io>
2025-04-21 10:09:48 +0200

    set up api test endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,31 @@
 import { Hono } from "jsr:@hono/hono";
+
 const app = new Hono();
-app.get("/", (c) => c.text("Hello again, World!"));
+
+// Endpoint 0: Check API connection
+app.get("/", (c) => c.text("Hello, World!"));
+
+// Endpoint 1: Retrieve details of a single SWIFT code whether for a headquarters or branches.
+app.get("/v1/swift-codes/:swiftCode", (c) => {
+  const swiftCode = c.req.param("swiftCode");
+  return c.text(swiftCode);
+});
+
+// Endpoint 2: Return all SWIFT codes with details for a specific country (both headquarters and branches).
+app.get("/v1/swift-codes/country/:ISO2code", (c) => {
+  const ISO2code = c.req.param("ISO2code");
+  return c.text(ISO2code);
+});
+
+// Endpoint 3: Adds new SWIFT code entries to the database for a specific country.
+app.post("/v1/swift-codes", (c) => {
+  return c.text("POST");
+});
+
+// Endpoint 4: Deletes swift-code data if swiftCode matches the one in the database.
+app.delete("/v1/swift-codes/:swiftCode", (c) => {
+  const swiftCode = c.req.param("swiftCode");
+  return c.text(`DELETE ${swiftCode}`);
+});
+
 Deno.serve({ port: 8080 }, app.fetch);
